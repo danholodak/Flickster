9423728328700545395a697ee07a562101286b36
Dan Holodak <danholodak@gmail.com>
2023-06-21 23:43:03 -0400

    albums page progress

--- frontend/src/components/AlbumListOther.jsx
+++ frontend/src/components/AlbumListOther.jsx
@@ -1,4 +1,23 @@
-
+import Header from "./Header";
+import { getPhotos, fetchPhotos } from "../store/photos";
+import { getAlbums, fetchAlbums } from "../store/albums";
 export default function AlbumListOther({user}){
+
+
+function clickAlbum(id){
+
+}
+    return (
+        <>
+        <Header state="loggedIn"/>
+        {user.albums.map((album)=>
+                    {<div key={i} className="square-photo-container">
+                        <p className="square-photo-title">{album.title}</p>
+                        <p className="square-photo-user">by {user.firstName} {user.lastName}</p>
+                        <img src={photos[album.header].img} alt={`${album.title} header`} className="square-photo" onClick={(e)=>clickAlbum(album.id)}/>
+                     </div>}
+        )}
+        </>
+    )
 
 }
