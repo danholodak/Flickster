b9cbad4ede3676954921559de95660538598a54b
Dan Holodak <danholodak@gmail.com>
2023-06-23 18:52:18 -0400

    Finish other albums page, start self albums

--- frontend/src/components/AlbumListOther.jsx
+++ frontend/src/components/AlbumListOther.jsx
@@ -1,20 +1,30 @@
-import Header from "./Header";
 import { getPhotos, fetchPhotos } from "../store/photos";
 import { getAlbums, fetchAlbums } from "../store/albums";
-export default function AlbumListOther({user}){
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
+export default function AlbumListOther({user}){
+    const history = useHistory()
+    const dispatch= useDispatch()
+    const albums = useSelector(getAlbums)
+    const photos = useSelector(getPhotos)
+    useEffect(()=>{
+        dispatch(fetchAlbums(user.id))
+        dispatch(fetchPhotos)
+    }, [user])
 
 function clickAlbum(id){
-
+    history.push(`/albums/${id}`)
 }
     return (
         <>
-        <Header state="loggedIn"/>
-        {user.albums.map((album)=>
+        {user.albums.map((album, i)=>
                     {<div key={i} className="square-photo-container">
-                        <p className="square-photo-title">{album.title}</p>
+                        <p className="square-photo-title">{albums[album].title}</p>
                         <p className="square-photo-user">by {user.firstName} {user.lastName}</p>
-                        <img src={photos[album.header].img} alt={`${album.title} header`} className="square-photo" onClick={(e)=>clickAlbum(album.id)}/>
+                        <img src={photos[albums[album].header].img} alt={`${album[album].title} header`} className="square-photo" onClick={(e)=>clickAlbum(album)}/>
                      </div>}
         )}
         </>
--- frontend/src/components/AlbumListSelf.jsx
+++ frontend/src/components/AlbumListSelf.jsx
@@ -1,5 +1,32 @@
-
+import { getPhotos, fetchPhotos } from "../store/photos";
+import { getAlbums, fetchAlbums } from "../store/albums";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function AlbumListSelf({user}){
+    const history = useHistory()
+    const dispatch= useDispatch()
+    const albums = useSelector(getAlbums)
+    const photos = useSelector(getPhotos)
+    useEffect(()=>{
+        dispatch(fetchAlbums(user.id))
+        dispatch(fetchPhotos)
+    }, [user])
 
+    function clickAlbum(id){
+        history.push(`/albums/${id}`)
+    }
+    return (
+        <>
+        {user.albums.map((album, i)=>
+                    {<div key={i} className="square-photo-container">
+                        <p className="square-photo-title">{albums[album].title}</p>
+                        <p className="square-photo-user">by {user.firstName} {user.lastName}</p>
+                        <img src={photos[albums[album].header].img} alt={`${album[album].title} header`} className="square-photo" onClick={(e)=>clickAlbum(album)}/>
+                     </div>}
+        )}
+        </>
+    )
 }
